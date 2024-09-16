3a7a4df7a144604a19721b6c611f4fc84e20ec90
Leandro Cavalcante <79013127+LeoScripts@users.noreply.github.com>
2024-09-16 15:05:04 -0300

    fix:  variable name R3.js

--- R3.js
+++ R3.js
@@ -1,10 +1,10 @@
 // coloquei 2 senarios onde o 1º nao temos faturamento no final de semana e no 2º temos faturamento no sabado
 // a contagem começa supondo que o 1º dia seja segunda-feira
-const faturamentoDiario = [
+const faturamentosDiarios = [
   150, 200, 10, 300, 1.5, 0, 0, 250, 5, 75, 500, 1000, 25, 0,
 ];
 
-const faturamentoFiltrado = faturamentoDiario.filter((valor) => valor > 0);
+const faturamentoFiltrado = faturamentosDiarios.filter((valor) => valor > 0);
 
 const menorFaturamento = Math.min(...faturamentoFiltrado);
 const maiorFaturamento = Math.max(...faturamentoFiltrado);
